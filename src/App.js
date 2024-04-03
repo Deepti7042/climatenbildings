260da5578d844766d2167597457499c8c25aeef7
Deepti <deeptituteja1911@gmail.com>
2024-04-03 16:05:39 -0300

    bar graph

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import CanadaMap from './Maps/CanadaMap';
-import CanadaTempBar from './BarChart/CanadaTemp Barchart';
-import RadialStackedBarChart from './BarChart/RadicalStackedChart';
-import CorrelationHeatmap from './Heatmap/CorrelationHeatMap';
 import HeatmapCorrelationData from './Heatmap/Heatmap';
 import data from '../src/Heatmap/filtered_heatmap_data.json'
+import BarGraph from './BarChart/CanadaTemp Barchart';
 
 function App() {
   return (
     <div>
-      <CanadaTempBar/>
+      <BarGraph/>
       <CanadaMap/>
       <HeatmapCorrelationData data={data}/>
     </div>
